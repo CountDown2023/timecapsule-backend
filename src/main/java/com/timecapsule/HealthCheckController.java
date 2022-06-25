7776c2c54e92f39c5ab66915f3af6eb1151786cd
dls4585 <dls4585@naver.com>
2022-06-25 17:34:39 +0900

    feat: HealthCheckController 생성

---
+++ src/main/java/com/timecapsule/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.timecapsule;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/ping")
+    public String healthCheck() {
+        return "pong";
+    }
+}
