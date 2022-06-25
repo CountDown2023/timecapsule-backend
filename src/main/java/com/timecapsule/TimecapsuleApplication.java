1320398784d1f6917c1bff3379741553e7e4c33f
dls4585 <dls4585@naver.com>
2022-06-25 18:17:50 +0900

    fix: 우선 DB 사용 안하도록 설정

--- src/main/java/com/timecapsule/TimecapsuleApplication.java
+++ src/main/java/com/timecapsule/TimecapsuleApplication.java
@@ -2,8 +2,9 @@ package com.timecapsule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class TimecapsuleApplication {
 
 	public static void main(String[] args) {
