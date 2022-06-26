ca8b3931b08c2c033b5fcf0bb7d86819f5c3ecc9
dls4585 <dls4585@naver.com>
2022-06-26 22:51:38 +0900

    chore: check style + intellij code style 적용

--- src/main/java/com/timecapsule/HealthCheckController.java
+++ src/main/java/com/timecapsule/HealthCheckController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
+
     @GetMapping("/ping")
     public String healthCheck() {
         return "pong";
--- src/main/java/com/timecapsule/TimecapsuleApplication.java
+++ src/main/java/com/timecapsule/TimecapsuleApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TimecapsuleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TimecapsuleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TimecapsuleApplication.class, args);
+    }
 
 }
