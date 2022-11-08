a95527eb67490a00f98afd7e7af9937fbce48206
dls4585 <dls4585@naver.com>
2022-11-09 00:29:39 +0900

    feat: password encode 해서 저장하도록 변경

--- common/timecapsule-database/src/main/kotlin/com/timecapsule/database/entity/Member.kt
+++ common/timecapsule-database/src/main/kotlin/com/timecapsule/database/entity/Member.kt
@@ -1,17 +1,15 @@
 package com.timecapsule.database.entity
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
 data class Member(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private val id: Long? = null,
-    @javax.persistence.Column(unique = true)
+    @Column(unique = true)
     private val nickName: String,
     private val password: String,
+    @Column(unique = true)
     private val email: String,
 ): BaseTimeEntity()
