c7b3c6a84895df3a6666c8fed24596d4adf5368d
dls4585 <dls4585@naver.com>
2022-11-11 00:51:14 +0900

    refactor: member 관련 request 하나로 모으기

---
+++ timecapsule-api/src/main/kotlin/com/timecapsule/api/dto/MemberRequest.kt
@@ -0,0 +1,24 @@
+package com.timecapsule.api.dto
+
+import javax.validation.constraints.Email
+
+sealed class MemberRequest(
+    open val nickname: String,
+)
+
+data class SignupRequest(
+    override val nickname: String,
+    val password: String,
+    @field:Email
+    val email: String,
+): MemberRequest(nickname)
+
+data class LoginRequest(
+    override val nickname: String,
+    val password: String,
+): MemberRequest(nickname)
+
+data class RefreshRequest(
+    override val nickname: String,
+    val refreshToken: String,
+): MemberRequest(nickname)
